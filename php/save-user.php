6c140f24f6ac35effaa58725c6e4a533f9f3f8b7
Misael <misaelmafc@gmail.com>
2021-01-26 00:57:05 -0300

    Cambio en los datos enviados para el registro, esto debido al cambio en la forma de registrarse

--- php/save-user.php
+++ php/save-user.php
@@ -9,16 +9,17 @@ $nacionalidad          = $_POST["rg_input6"];
 $email                 = $_POST["rg_input7"];
 $telefono              = $_POST["rg_input8"];
 $prevision             = $_POST["rg_input9"];
-$contrasena            = sha1($_POST["rg_input10"]);  
+//$contrasena            = sha1($_POST["rg_input10"]);  
 $date_save             = date("Y-m-d H:i:s");
+//$token                 = md5($_POST["rg_input1"]+date("Y-m-d H:i:s"));
 
 include("conexion.php");
 
 $sql =
-    "INSERT INTO usuarios 
-    (numero_identificacion, nombre, apellidos, fecha_nacimiento, sexo, nacionalidad, email, telefono, prevision, contrasena, date_save)
+    "INSERT INTO pacientes 
+    (numero_identificacion, nombre, apellidos, fecha_nacimiento, sexo, nacionalidad, email, telefono, prevision, date_save)
     VALUES 
-    ('$numero_identificacion', '$nombre', '$apellidos', '$fecha_nacimiento', '$sexo', '$nacionalidad', '$email', '$telefono', '$prevision', '$contrasena', '$date_save')";
+    ('$numero_identificacion', '$nombre', '$apellidos', '$fecha_nacimiento', '$sexo', '$nacionalidad', '$email', '$telefono', '$prevision', '$date_save')";
 
 echo mysqli_query($link, $sql);
 
